c28911a6c8fe210e9ebc0c5665056fb12625d13f
mgmkei <rstoyo3.mizugami.keisuke@techcamp-training.com>
2024-04-25 13:41:45 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+class PostsController < ApplicationController
+  def index #indexアクションの定義
+    @posts = Post.all
+  end
+
+  def new
+    
+  end
+
+  def create
+    Post.create(content: params[:content])
+    redirect_to "/posts"
+  end
+end
